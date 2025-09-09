de08ed8e5586756155d84657e84a387f3777e2de
Dilara <dilara.dcelik@gmail.com>
2025-09-09 23:35:26 +0300

    add styling

--- dilara_celik.js
+++ dilara_celik.js
@@ -4,6 +4,9 @@ const PRODUCT_URL = "https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4ab
 async function carouselSection() {
 
     const productBannerCarousel = document.querySelector("eb-product-carousel")
+    const customProductCarousel = document.querySelector(".custom-product-carousel")
+
+    if(customProductCarousel) customProductCarousel.remove()
 
     const productCarousel = document.createElement('div')
     productCarousel.classList.add('custom-product-carousel')
@@ -11,12 +14,13 @@ async function carouselSection() {
     const eBebekProducts = await fetchProducts()
     productCarousel.innerHTML = buildHTML(eBebekProducts)
 
-
-
     productBannerCarousel.before(productCarousel)
 
 }
 
+function addFavourite(){
+
+}
 
 async function fetchProducts() {
     let eBebekProducts = localStorage.getItem("e_bebek_products")
@@ -55,28 +59,14 @@ const buildHTML = (eBebekProducts) => {
                                 <div class="product-itself">
                                     <a href=${product.url} target="_blank">
                                         <div class="product-info">
-                                            <div class="product-content">
-                                                <div class="product-tags">çok satar</div>
-                                                <div class="product-media">
-                                                    <figure>
-                                                        <img style="width: 100px; height:auto" src="${product.img}"/>
-                                                    </figure>
-                                                </div>
+                                            <div class="product-media">
+                                                    <img src="${product.img}"/>
                                             </div>
                                             <div class="product-name-info">
                                                 <h2 class="product-name">
                                                     <b>${product.brand} - </b>
                                                     <span>${product.name}</span>
                                                 </h2>
-                                                <div class="product-ratio">
-                                                    <cx-star-rating>
-                                                        <cx-icon>
-                                                        </cx-icon>
-                                                        <cx-icon>
-                                                        </cx-icon>
-                                                    </cx-star-rating>
-                                                    <p class="rev-count">(5)</p>
-                                                </div>
                                             </div>
                                         </div>
                                         <div class="product-price-wrapper">
@@ -92,10 +82,15 @@ const buildHTML = (eBebekProducts) => {
                                     </a>
                                 </div>
                                 <div class="add-to-wishlist">
-
+                                    <div class="heart">
+                                        <div class="icon-wrapper">
+                                            <i class="toys-icon toys-icon-heart-outline"></i>
+                                            <i class="toys-icon toys-icon-heart-orange-outline hovered"></i>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="add-to-cart">
-                                    <button id="addToCartBtn" type="submit" class="btn btn-add disable btn-add-circle ng-star-inserted">
+                                    <button id="addToCartBtn" class="btn btn-add disable btn-add-circle ng-star-inserted">
                                         <div class="inner-btn ng-star-inserted">
                                             <i class="toys-icon toys-icon-plus-blue add-icon"></i>
                                                 <i class="toys-icon toys-icon-plus-white add-icon hovered"></i>
@@ -105,14 +100,13 @@ const buildHTML = (eBebekProducts) => {
                             </div>
                             `
                             ).join('')}
-                            
-                            <button aria-label="back" class="swiper-prev">
-                                <i class="toys-icon toys-icon-arrow-left"></i>
-                            </button>
-                            <button aria-label="next" class="swiper-next">
-                                <i class="toys-icon toys-icon-arrow-right"></i>
-                            </button>
                         </div>
+                        <button aria-label="back" class="swiper-prev">
+                            <i class="toys-icon toys-icon-arrow-left"></i>
+                        </button>
+                        <button aria-label="next" class="swiper-next">
+                            <i class="toys-icon toys-icon-arrow-right"></i>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -120,4 +114,189 @@ const buildHTML = (eBebekProducts) => {
     `
     return html
 }
+
+const buildCSS = () => {
+    const css = 
+    `
+   .Section2A custom-product-crausel {
+        padding-bottom: 50px;
+    }
+
+    .custom-banner-title {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-family: Quicksand-SemiBold;
+        font-weight: 700;
+    }
+
+    .custom-banner-title h2 {
+        color: var(--Primary-Black);
+        font-size: 24px;
+        margin: 0;
+    }
+
+    .custom-banner-products {
+        box-shadow: none;
+    }
+
+    .custom-banner-products {
+        position: relative;
+        background-color: #fff;
+    }
+
+    .product-list-wrapper {
+        width: 100%;
+        -webkit-tap-highlight-color: transparent;
+        position: relative;
+        z-index: 1;
+    }
+
+    .product-list-wrapper .product-list {
+        position: relative;
+        overflow: hidden;
+        -webkit-transform: translateZ(0);
+        touch-action: manipulation;
+    }
+
+    .banner .product-list {
+        display: flex;
+        padding-bottom: 20px;
+        padding-top: 20px;
+    }
+    .banner .swiper-prev {
+        left: -65px;
+        bottom: 50%;
+        top: auto;
+    }
+    .banner .swiper-next {
+        right: -65px;
+        bottom: 50%;
+        top: auto;
+    }
+
+    .product-wrapper .heart {
+        right: 0;
+        top: 0;
+        z-index: 2;
+    }
+    .product-wrapper .add-to-cart {
+        z-index: 2;
+        position: absolute;
+        bottom: 4px;
+        right: 4px;
+    }
+    .product-list-wrapper .product-wrapper {
+        position: relative;
+        min-height: 1px;
+        float: left;
+        -webkit-backface-visibility: hidden;
+        -webkit-tap-highlight-color: transparent;
+        min-width: 245.2px;
+        margin-right: 16px;
+    }
+    .product-itself {
+        z-index: 1;
+        display: block;
+        width: 100%;
+        font-family: Quicksand-Medium;
+        font-size: 12px;
+        border: 1px solid var(--Neutral-200);
+        border-radius: 8px;
+        position: relative;
+        text-decoration: none;
+        background-color: var(--Neutral-0---White);
+        overflow: hidden;
+        height: 100%;
+        color: unset;
+        background: unset;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+    
+    .product-itself:focus, .product-itself:hover {
+        color: unset;
+        outline: none;
+        border:1px solid var(--Neutral-500,#c1ccd4)
+    }
+    
+    .product-itself:active {
+        border:1px solid var(--Neutral-500,#c1ccd4)
+    }
+    .product-name-info {
+        padding-bottom: 13px;
+        padding: 0 10px;
+    }
+    .product-name{
+        font-size: 12px;
+        color: var(--Primary-Black);
+        text-overflow: ellipsis;
+        overflow: hidden;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        display: -webkit-box;
+        margin-bottom: 10px;
+    }
+    .product-price-wrapper {
+        position: relative;
+        display: flex;
+        justify-content: flex-end;
+        flex-direction: column;
+        padding: 6px 10px 15px;
+    }
+    .product-price {
+        font-family: Quicksand-SemiBold;
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        color: var(--Neutral-600);
+    }
+    .product-media {
+        position: relative;
+        display: block;
+        width: 100%;
+        background-color: #fff;
+        text-align: center;
+        margin: 0 0 1rem;
+    }
+    .product-media img{
+        display: inline-block;
+        max-height: 60%;
+        border-radius: 10px;
+        background-color: #fff;
+        width: 100%;
+        height: 203px;
+        object-fit: contain;
+    }
+
+    @media (max-width: 1480px) {
+        .product-list-wrapper .product-wrapper {
+            min-width: 275.5px;
+        }
+    }
+     /* Responsive Media Queries */
+    @media (max-width: 1280px) {
+        .product-list-wrapper .product-wrapper {
+            min-width: 299.333px;
+        }
+    }
+
+    @media (max-width: 992px) {
+        .product-list-wrapper .product-wrapper {
+            min-width: 337px;
+        }
+    }
+    
+    @media (max-width: 768px) {
+        .product-list-wrapper .product-wrapper {
+            min-width: 247px;
+        }
+    }
+    `
+    const style = document.createElement("style");
+    style.textContent = css;
+    document.head.appendChild(style);
+}
+buildCSS()
 carouselSection()
